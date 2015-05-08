48706422ea7b3fcd5c66d7b872b55bb989addf67
Jennifer <jbolanos707@gmail.com>
2015-05-08 13:22:38 -0700

    update the clients name in the database

--- lib/client.rb
+++ lib/client.rb
@@ -1,14 +1,15 @@
 class Client
 
-  attr_reader(:name, :stylist_id)
+  attr_reader(:name, :id, :stylist_id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
     @stylist_id = attributes.fetch(:stylist_id)
   end
 
   define_method(:==) do |another_client|
-    self.name.==(another_client.name).&(self.stylist_id.==(another_client.stylist_id))
+    self.name.==(another_client.name).&(self.id.==(another_client.id))
   end
 
   define_singleton_method(:all) do
@@ -16,17 +17,23 @@ class Client
     clients = []
     returned_clients.each do |client|
       name = client.fetch('name')
+      id = client.fetch('id').to_i
       stylist_id = client.fetch("stylist_id").to_i
-      clients.push(Client.new(name: name, stylist_id: stylist_id))
+      clients.push(Client.new(name: name, id: id, stylist_id: stylist_id))
     end
     clients
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}', #{@stylist_id});")
+    result = DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}',#{@stylist_id}) RETURNING id;")
+    @id = result.first.fetch("id").to_i
   end
 
-  
+  define_method(:update) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = self.id
+    DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{@id};")
+  end
 
 
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,15 +4,15 @@ describe(Client) do
 
   describe("name") do
     it("tells you the name fo the client") do
-      client = Client.new(name: "Jennifer", stylist_id: 1)
+      client = Client.new(name: "Jennifer", id: nil, stylist_id: 1)
       expect(client.name).to eq("Jennifer")
     end
   end
 
   describe("#==") do
     it("is the same client if the name is the same") do
-      client1 = Client.new(name: "Jennifer", stylist_id: 1)
-      client2 = Client.new(name: "Jennifer", stylist_id: 1)
+      client1 = Client.new(name: "Jennifer", id: nil, stylist_id: 1)
+      client2 = Client.new(name: "Jennifer", id: nil, stylist_id: 1)
       expect(client1).to eq(client2)
     end
   end
@@ -25,7 +25,7 @@ describe(Client) do
 
   describe("#save") do
     it("adds a client to the array of saved clients") do
-      test_client = Client.new(name: "Jennifer", stylist_id: 1)
+      test_client = Client.new(name: "Jennifer", id: nil, stylist_id: 1)
       test_client.save
       expect(Client.all).to eq([test_client])
     end
@@ -33,8 +33,17 @@ describe(Client) do
 
   describe("#stylist_id") do #this method will assign clients to a stylsit. when this method is implementd stylist_id is added as parameter to the class Client)
     it("lets you read the list ID out") do
-      test_client = Client.new(name: "Jennifer", stylist_id: 1)
+      test_client = Client.new(name: "Jennifer", id: nil, stylist_id: 1)
       expect(test_client.stylist_id).to eq(1)
+    end
+  end
+
+  describe("#update") do
+    it("lets you update clients in the database") do
+      test_client = Client.new(name: "Jennifer", id: nil, stylist_id: 1)
+      test_client.save
+      test_client.update(name: "Carol")
+      expect(test_client.name).to eq("Carol")
     end
   end
 
