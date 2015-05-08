f270d8fd4bae6ddf2368b44044d0f8f82a06c52b
Jennifer <jbolanos707@gmail.com>
2015-05-08 12:26:45 -0700

    add/assign stylists to the stylist_id column in the clients table in the hair_salon database

--- lib/client.rb
+++ lib/client.rb
@@ -1,13 +1,14 @@
 class Client
 
-  attr_reader(:name)
+  attr_reader(:name, :stylist_id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
+    @stylist_id = attributes.fetch(:stylist_id)
   end
 
   define_method(:==) do |another_client|
-    self.name.==(another_client.name)
+    self.name.==(another_client.name).&(self.stylist_id.==(another_client.stylist_id))
   end
 
   define_singleton_method(:all) do
@@ -15,13 +16,14 @@ class Client
     clients = []
     returned_clients.each do |client|
       name = client.fetch('name')
-      clients.push(Client.new(name: name))
+      stylist_id = client.fetch("stylist_id").to_i
+      clients.push(Client.new(name: name, stylist_id: stylist_id))
     end
     clients
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO clients (name) VALUES ('#{@name}')")
+    DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}', #{@stylist_id});")
   end
 
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,15 +4,15 @@ describe(Client) do
 
   describe("name") do
     it("tells you the name fo the client") do
-      client = Client.new(name: "Jennifer", id: nil)
+      client = Client.new(name: "Jennifer", stylist_id: 1)
       expect(client.name).to eq("Jennifer")
     end
   end
 
   describe("#==") do
     it("is the same client if the name is the same") do
-      client1 = Client.new(name: "Jennifer")
-      client2 = Client.new(name: "Jennifer")
+      client1 = Client.new(name: "Jennifer", stylist_id: 1)
+      client2 = Client.new(name: "Jennifer", stylist_id: 1)
       expect(client1).to eq(client2)
     end
   end
@@ -25,9 +25,16 @@ describe(Client) do
 
   describe("#save") do
     it("adds a client to the array of saved clients") do
-      test_client = Client.new(name: "Jennifer")
+      test_client = Client.new(name: "Jennifer", stylist_id: 1)
       test_client.save
       expect(Client.all).to eq([test_client])
+    end
+  end
+
+  describe("#stylist_id") do #this method will assign clients to a stylsit. when this method is implementd stylist_id is added as parameter to the class Client)
+    it("lets you read the list ID out") do
+      test_client = Client.new(name: "Jennifer", stylist_id: 1)
+      expect(test_client.stylist_id).to eq(1)
     end
   end
 
